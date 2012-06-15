fc40c52b73d4b20cd72860f8b3487d98aaa44616
Sibi <psibi2000@gmail.com>
2012-06-15 11:49:31 +0530

    Updates Status in identi.ca

---
+++ update_identica.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from identi import Identica
+import sys
+
+status_text = ""
+if (len(sys.argv) > 1):
+    for i in range(len(sys.argv)-1):
+        status_text = status_text + sys.argv[i+1]
+        status_text = status_text + " "
+else:
+    print """Usage: tw "Enter your status here\""""
+    sys.exit(1)
+
+consumer_key ="xxxxxxxxxxxx"
+consumer_secret ="xxxxxxxxx"
+oauth_token = "xxxxxxxxxxxxx"
+oauth_secret = "xxxxxxxxxxx"
+identica = Identica(consumer_key,consumer_secret,oauth_token,oauth_secret)
+identica.updateStatus(status=status_text)
