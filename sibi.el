10410454e04373f0413c3c84c54db19a095d79ff
Sibi <sibi@psibi.in>
2013-01-11 16:36:09 +0530

    Update sibi.el

--- sibi.el
+++ sibi.el
@@ -6,3 +6,10 @@
 (require 'ansi-color)
 (require 'recentf)
 
+;;Install ecb-snapshot from M-x package-list-packages
+(require 'ecb)
+(setq stack-trace-on-error t)
+
+;;Install Window-number mode - Use M-1,M-2 to jump between windows
+(require 'window-number)
+(window-number-meta-mode)
