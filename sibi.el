d7427fec4b4cd95974ed1c22eb2e44905279bee6
Sibi <sibi@psibi.in>
2013-01-17 13:48:12 +0530

    Updated. Included features:
    1) Maximize
    2) Full Screen
    3) Refresh Buffer.
    4) Removed Menu Bar & Tool Bar.

--- sibi.el
+++ sibi.el
@@ -6,6 +6,9 @@
 (require 'ansi-color)
 (require 'recentf)
 
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
 ;;Install ecb-snapshot from M-x package-list-packages
 (require 'ecb)
 (setq stack-trace-on-error t)
@@ -13,3 +16,23 @@
 ;;Install Window-number mode - Use M-1,M-2 to jump between windows
 (require 'window-number)
 (window-number-meta-mode)
+
+;;Fullscreen mode - Press M-x fullscreen for switching to Fullscreen mode.
+(defun fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                         '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
+
+;;Maximize Screen
+(defun maximize (&optional f)
+       (interactive)
+       (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+       (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+
+(maximize)
+
+;; Bind C-c C-r key for refreshing of a file
+(global-set-key (kbd "C-c C-r") 'revert-buffer)
+
