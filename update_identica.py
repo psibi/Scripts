b3060003110ebcedb6f9cbc54fc955e1270dfd55
Sibi <sibi@psibi.in>
2013-07-14 02:18:21 +0530

    * update_identica.py: Sync both to twitter and pump.io services now.

--- update_identica.py
+++ update_identica.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-from identi import Identica
+from pypump import PyPump
 import sys
+import tweepy
 
 status_text = ""
 if (len(sys.argv) > 1):
@@ -15,9 +16,42 @@ else:
     print """Usage: tw "Enter your status here\""""
     sys.exit(1)
 
-consumer_key ="xxxxxxxxxxxx"
-consumer_secret ="xxxxxxxxx"
-oauth_token = "xxxxxxxxxxxxx"
-oauth_secret = "xxxxxxxxxxx"
-identica = Identica(consumer_key,consumer_secret,oauth_token,oauth_secret)
-identica.updateStatus(status=status_text)
+consumer_key =u''
+consumer_secret = u''
+oauth_token = ''
+oauth_secret = ''
+
+pump = PyPump("psibi@identi.ca", key=consumer_key, secret=consumer_secret, token=oauth_token, token_secret=oauth_secret)
+note = pump.Note(status_text)
+note.send()
+
+
+# == OAuth Authentication ==
+#
+# This mode of authentication is the new preferred way
+# of authenticating with Twitter.
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+consumer_key=""
+consumer_secret=""
+
+# The access tokens can be found on your applications's Details
+# page located at https://dev.twitter.com/apps (located 
+# under "Your access token")
+access_token=""
+access_token_secret=""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# If the authentication was successful, you should
+# see the name of the account print out
+print api.me().name
+
+# If the application settings are set for "Read and Write" then
+# this line should tweet out the message to your account's 
+# timeline. The "Read and Write" setting is on https://dev.twitter.com/apps
+api.update_status(status_text)
